228ae110bb8d63037621ca4951163374a103da79
Marielena-GS <mgonzalez@uce.edu.ec>
2024-12-13 23:05:52 -0500

    User

--- src/main/java/ec/edu/uce/jpa/User.java
+++ src/main/java/ec/edu/uce/jpa/User.java
@@ -13,4 +13,30 @@ public class User {
     private String name;
     @Column(name= "User_Last_Name")
     private String lastName;
+
+    public User() { }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
