83eedd7c3f9a9ee9a7402738e7b8e45863508485
Marielena-GS <mgonzalez@uce.edu.ec>
2024-12-13 23:03:52 -0500

    State

--- src/main/java/ec/edu/uce/jpa/State.java
+++ src/main/java/ec/edu/uce/jpa/State.java
@@ -1,9 +1,32 @@
 package ec.edu.uce.jpa;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "State")
 public class State {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "State_Product")
+    private String state_product;
+
+    public State() { }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getState_product() {
+        return state_product;
+    }
+
+    public void setState_product(String state_product) {
+        this.state_product = state_product;
+    }
 }
