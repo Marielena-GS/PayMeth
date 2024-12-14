4421e5ca07dfc1d508cc5b34dafb0403f02d3da9
Marielena-GS <mgonzalez@uce.edu.ec>
2024-12-13 23:26:31 -0500

    UsersServiceCreate

--- src/main/java/ec/edu/uce/jpa/UsersService.java
+++ src/main/java/ec/edu/uce/jpa/UsersService.java
@@ -1,4 +1,40 @@
 package ec.edu.uce.jpa;
 
+import jakarta.persistence.EntityManager;
+
 public class UsersService {
+
+    private EntityManager em;
+
+    public UsersService(EntityManager em)
+    {
+        this.em = em;
+    }
+
+    public User createUser(int id, String name, String lastName) {
+        User user = new User();
+        user.setId(id);
+        user.setName(name);
+        user.setLastName(lastName);
+        em.persist(user);//actualiza todos los datos
+        return user;
+    }
+
+    public User findByID(int id) {
+        return em.find(User.class, id);
+    }
+
+
+    public void updateUser(User user) {
+        em.getTransaction().begin();
+        em.merge(user);
+        em.getTransaction().commit();
+    }
+
+    public void deleteById(int id) {
+        User user = findByID(id);
+        em.getTransaction().begin();
+        em.remove(user);
+        em.getTransaction().commit();
+    }
 }
